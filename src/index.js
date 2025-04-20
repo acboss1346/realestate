eea8720e7c2b247df206d953c4ee8f27ea4414d0
AkshatChauhan <akshatchauhan1346@gmail.com>
2025-04-20 21:49:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; // Importing CSS styles
+import App from './App'; // Importing the main App component
+import reportWebVitals from './reportWebVitals'; // For measuring performance
+import { BrowserRouter } from 'react-router-dom'; // Importing BrowserRouter for routing
 
+// Creating a root element to render the React application into the DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendering the App component wrapped in BrowserRouter for handling routing
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> {/* This is an optional tool that helps identify potential problems in the app during development */}
+    <BrowserRouter> {/* BrowserRouter allows the app to handle client-side routing */}
+      <App /> {/* The main App component */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance in the app. The reportWebVitals function can be passed
+// a callback to log or send data elsewhere (e.g., to analytics).
 reportWebVitals();
