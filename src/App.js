dc579e1f901fd341573f0a4140bf9a746e121d90
Akshat Chauhan <akshatchauhan1346@gmail.com>
2025-04-20 22:23:36 +0530

    Final conflict fix and cleanup

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
-<<<<<<< HEAD
-import { Route, Routes } from "react-router-dom";
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from './routes/Home';
 import About from './routes/About';
 import Service from './routes/Service';
 import Contact from './routes/Contact';
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> f378117ea08f911f82cae92847b6502642da2624
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> f378117ea08f911f82cae92847b6502642da2624
     </div>
   );
 }
